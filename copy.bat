8627b25e4907495a7dd515cf6fd57cdae7f3c770
George <20459298+george-chou@users.noreply.github.com>
2022-04-01 20:08:12 +0800

    adjust dir structure & add copy

---
+++ copy.bat
@@ -0,0 +1,2 @@
+xcopy crack.bat "H:\crack" /y
+pause
--- crack.bat
+++ crack.bat
@@ -1,4 +1,4 @@
 d:
 cd D:\hashcat-6.2.5
-hashcat.exe -a 3 -m 22000 -D 2 -w 2 E:\workspace\crack\yjkj.hc22000 1?d?d?d?d?d?d?d?d?d?d
+hashcat.exe -a 3 -m 22000 -D 2 -w 2 H:\crack\*\*.hc22000 1?d?d?d?d?d?d?d?d?d?d
 pause
